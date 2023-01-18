7c8c92764fb530fc3a6e3ba2c1a48d00cff6bf13
lllchak <mediumchak@yandex.ru>
2023-01-18 17:10:41 +0300

    implemented check with printing & with results comparison

--- utils/run_tests.py
+++ utils/run_tests.py
@@ -1,10 +1,11 @@
 from utils import dtypes
 
 
-def check(func: dtypes.Callable, ground_truth: dtypes.Any, **kwargs) -> None:
+def check_print(func: dtypes.Callable, ground_truth: dtypes.Any, **kwargs) -> None:
     """
     Testing tasks function. Receives a function to check with its
-    argumens and an expected answer. Simply print out your answer and expected one.
+    argumens and an expected answer. Since answers can be in any order 
+    simply print out your answer and expected one.
 
     Args:
         func (dtypes.Callable)    : Function to check
@@ -14,4 +15,19 @@ def check(func: dtypes.Callable, ground_truth: dtypes.Any, **kwargs) -> None:
 
     print(f"Function result:\n\t{func(**kwargs)}")
     print(f"Expected answer\n\t{ground_truth}")
+
+
+def check_eq(func: dtypes.Callable, ground_truth: dtypes.Any, **kwargs) -> bool:
+    """
+    Testing tasks function. Receives a function to check with its
+    argumens and an expected answer. Returns bool flag if func output
+    mathes the expected one
+
+    Args:
+        func (dtypes.Callable)    : Function to check
+        ground_truth (dtypes.Any) : Expected answer
+        **kwargs                  : Function to check parameters
+    """
+    
+    return ground_truth == func(**kwargs)
     

3acb4b7c46f5dbdb239d0a33d6378090ba3a201b
lllchak <mediumchak@yandex.ru>
2023-01-18 17:30:41 +0300

    added data structures docs

--- utils/data_structures.py
+++ utils/data_structures.py
@@ -2,6 +2,15 @@ import dtypes
 
 
 class ListNode:
+    """
+    Linked list node class. Stores node value with the pointer
+    to the next node.
+
+    Usage example:
+        my_list = ListNode(1, ListNode(5, ListNode(3, None)))
+        my_list = 1 -> 5 -> 3 -> NULL
+    """
+
     def __init__(
         self,
         val: dtypes.Any = None,
@@ -12,6 +21,25 @@ class ListNode:
 
 
 class TreeNode:
+    """
+    Binary tree node class. Stores node value with the pointers
+    to the left and right subtrees.
+
+    Usage example:
+        my_tree = TreeNode(
+            1, TreeNode(
+                3, TreeNode(
+                    2, None
+                ), TreeNode(
+                    6, None
+                )
+            ), TreeNode(
+                4, None
+            ), TreeNode(
+                7, None
+            )
+        )
+    """
     def __init__(
         self, 
         val: dtypes.Any = None,
@@ -24,6 +52,16 @@ class TreeNode:
 
 
 class N_aryTreeNode:
+    """
+    N-ary Binary tree node class. Stores node value and its children
+    as a list of nodes.
+
+    Usage example:
+        my_tree = N_aryTreeNode(
+            1, [N_aryTreeNode(3, None), N_aryTreeNode(6, None), N_aryTreeNode(1, None)]
+        )
+    """
+
     def __init__(
         self, 
         val: dtypes.Any = None,
